137805f5bb9da71be6993c8f0dc6b9bf1e7f3a52
sami kareem <103708057+Sami-Kareem@users.noreply.github.com>
2022-04-28 13:58:47 +0500

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+   import java.util.Scanner;
+class EvenOdd{
+		
+		public static void main (String args []){
+		
+		Scanner ob=new Scanner(System.in);
+			System.out.println("Hellow there! Enter any number  ");
+				int num=ob.nextInt();
+			if (num/2==0){
+				System.out.println("Even number it is= "+num);}
+			else  
+				System.out.println("its odd=  "+num);}}
